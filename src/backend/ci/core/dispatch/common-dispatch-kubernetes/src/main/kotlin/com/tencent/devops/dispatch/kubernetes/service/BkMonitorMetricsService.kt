a74c801d0089fbd9443386ff78e8d31e95cd7756
sawyersong <sawyersong@tencent.com>
2024-07-31 10:54:50 +0800

    feat：优化k8s构建集群方案 #10636

--- src/backend/ci/core/dispatch/common-dispatch-kubernetes/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/service/BkMonitorMetricsService.kt
+++ src/backend/ci/core/dispatch/common-dispatch-kubernetes/src/main/kotlin/com/tencent/devops/dispatch/kubernetes/service/BkMonitorMetricsService.kt
@@ -269,8 +269,8 @@ class BkMonitorMetricsService @Autowired constructor(
                 )
             ),
             expression = "",
-            startTime = startTime,
-            endTime = endTime,
+            startTime = 1722390808,
+            endTime = 1722394408,
             slimit = 500,
             downSampleRange = "2s"
         )
